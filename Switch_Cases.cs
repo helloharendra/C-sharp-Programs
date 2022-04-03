cccc36a00d8b3ae84f4ba92aae535f51961cdb7f
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-04-04 03:49:19 +0530

    Add files via upload

---
+++ Switch_Cases.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace witch_Case
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Number");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch(num)
+            {
+                case 1: Console.WriteLine("it is : "+num);
+                    break;
+                case 2: Console.WriteLine("it is : "+num);
+                    break;
+                case 3: Console.WriteLine("it is : "+num);
+                    break;
+                default:Console.WriteLine("not a  number in option please enter 1,2 or 3 ");
+                    break;
+            }
+        }
+    }
+}
