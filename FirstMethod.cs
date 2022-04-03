d9b08b16b174c19dcb3a776362bc9bbefd8c3085
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-04-04 02:04:47 +0530

    Add files via upload

---
+++ FirstMethod.cs
@@ -0,0 +1,10 @@
+﻿
+class FirstMethod        
+    {
+        static void Main(string[] args)
+        {
+        System.Console.WriteLine("This is first method to run c# Program");
+        System.Console.ReadLine(); //used to hold the screen/output 
+        }
+    }
+// This is simple program By @harendraprajapati72 
