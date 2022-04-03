73c877317710b8369f8b83673b705077e65afd6d
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-04-04 02:06:00 +0530

    Add files via upload

---
+++ FourthMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FourthProgram
+{
+    class FourthProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is 4th mathod to run C# program :");
+            Console.ReadLine();
+        }
+    }
+}
+//this is use of namespace in  c# program  by @harendraprajapati72
