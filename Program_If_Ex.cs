8c4cf66a5d4670b7fe22a71d87b5fc23d9261a5a
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-04-04 02:50:52 +0530

    Add files via upload

---
+++ Program_If_Ex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program_If_Ex
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i = 10;
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("number is Even");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
