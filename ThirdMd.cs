c654ce8fb10f240dd0583fec65a9dda17c1d5373
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-04-04 02:10:18 +0530

    Add files via upload

---
+++ ThirdMd.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    public class ThirdMethod
+
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("this is third program to run C# program ");
+        }
+    }
+//this is use of public modifier in c# program by @harendraprajapati72
