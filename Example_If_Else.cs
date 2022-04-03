bd400aacac5af1f9a88ae800c4d9b7747d1eaead
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-04-04 02:58:16 +0530

    Add files via upload

---
+++ Example_If_Else.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example_If_Else
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i = 15;
+            if (i% 2 ==0) {
+                Console.WriteLine(" number is even");
+            }else
+            {
+                Console.WriteLine(" number is odd");
+            }
+        }
+    }
+}
