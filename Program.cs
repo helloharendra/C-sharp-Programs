d2bb44ddbfcd574cc429711efe538ca0b16498f4
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-04-12 22:53:54 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 
-namespace FourthProgram
+namespace Harendra
 {
-    class FourthProgram
+    class Prajapati
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("This is 4th mathod to run C# program :");
-            Console.ReadLine();
+            
+            Console.WriteLine("Please select from the following");
+            Console.WriteLine("1.Red");
+            Console.WriteLine("2.Green");
+            Console.WriteLine("3.Yellow");
+            Console.WriteLine("Enter your choice");
+            int ch=Convert.ToInt16(Console.ReadLine());
+            switch(ch){
+                case 1: Console.WriteLine("You Selected : Red " );
+                    break;
+                case 2:
+                    Console.WriteLine("You Selected : Greed");
+                    break;
+                case 3:
+                    Console.WriteLine("You Selected : Yellow ");
+                    break;
+                default: Console.WriteLine("Wrong Selection Try again");
+                    break;
+            }
+           
+            }
         }
     }
-}
-//this is use of namespace in  c# program  by @harendraprajapati72
